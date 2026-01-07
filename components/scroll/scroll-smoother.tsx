28fe28a8fb5809d2583d0b83de1ddb2a49449531
Jono <jonathan.cole.1994@gmail.com>
2026-01-07 12:45:46 +0100

    smooth mobile test

--- components/scroll/scroll-smoother.tsx
+++ components/scroll/scroll-smoother.tsx
@@ -36,9 +36,9 @@ export default function SmoothScroller({ children }: { children: React.ReactNode
       wrapper: wrapperRef.current!,
       content: contentRef.current!,
       smooth: 0.5,
-      smoothTouch: 0,
+      smoothTouch: 0.5,
       effects: true,
-      normalizeScroll: true,
+      normalizeScroll: false,
     });
 
     if (contentRef.current) {
